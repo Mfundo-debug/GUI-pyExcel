fadd668217bd8ac430dfb1ef6cedf374328f1b02
Mfundo-debug <diditmfundo@gmail.com>
2023-08-07 12:29:04 +0200

    initial commit with basic functionality

---
+++ py_gui.py
@@ -0,0 +1,143 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog, messagebox
+import openpyxl as xl
+from openpyxl import Workbook
+#create the main window
+root = tk.Tk()
+root.title("pyExcel")
+
+#set the theme
+style = ttk.Style()
+style.theme_use('clam')
+
+#create the data entry form
+form_frame = ttk.LabelFrame(root,padding=20, text="Data Entry Form")
+form_frame.grid(row=0,column=0,sticky="NSEW")
+
+#create the data display frame
+display_frame = ttk.LabelFrame(root,padding=20, text="Data Display")
+display_frame.grid(row=0,column=1,sticky="NSEW")
+
+#Add labels and entry fields to the form
+name_label = ttk.Label(form_frame,text="Name:")
+name_label.grid(row=0,column=0,sticky="W")
+name_entry = ttk.Entry(form_frame,width=20)
+name_entry.grid(row=0,column=1,sticky="W")
+
+age_label = ttk.Label(form_frame,text="Age:")
+age_label.grid(row=1,column=0,sticky="W")
+age_entry = ttk.Entry(form_frame,width=20)
+age_entry.grid(row=1,column=1,sticky="W")
+
+gender_label = ttk.Label(form_frame, text="Gender:")
+gender_label.grid(row=2,column=0,sticky="W")
+gender_combo = ttk.Combobox(form_frame, values=['Male','Female','Other'], width=17, state="readonly")
+gender_combo.grid(row=2,column=1,sticky="W")
+
+hobbies_label = ttk.Label(form_frame,text="Hobbies:")
+hobbies_label.grid(row=3,column=0,sticky="W")
+hobbies_entry = ttk.Entry(form_frame,width=20)
+hobbies_entry.grid(row=3,column=1,sticky="W")
+
+#Add a checkbox to the form
+agree_var = tk.BooleanVar()
+agree_check = ttk.Checkbutton(form_frame,text="I agree to the terms and conditions",variable=agree_var)
+agree_check.grid(row=4,columnspan=2,sticky="W")
+
+#Add a button to submit the form
+def submit_form():
+    #get values from the form
+    name = name_entry.get()
+    age = age_entry.get()
+    gender = gender_combo.get()
+    hobbies = hobbies_entry.get()
+    agree = agree_var.get()
+
+    #validate the form
+    if not name:
+        messagebox.showerror("Error","Please enter a name")
+        return
+    if not age:
+        messagebox.showerror("Error","Please enter an age")
+        return
+    if not gender:
+        messagebox.showerror("Error","Please select a gender")
+        return
+    if not agree:
+        messagebox.showerror("Error","Please agree to the terms and conditions")
+        return
+    
+    #Add the data to the excel file
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Data"
+    ws.append(['Name','Age','Gender','Hobbies'])
+    wb.save("data.xlsx")
+
+    #show a success message
+    messagebox.showinfo("Success","Data added successfully")
+
+submit_button = ttk.Button(form_frame,text="Submit",command=submit_form)
+submit_button.grid(row=5,columnspan=2,sticky="EW")
+
+#Add a dark mode and light mode
+def set_dark_mode():
+    style.theme_use('clam')
+    root.configure(bg="#333")
+    form_frame.configure(bg="#333")
+    for widget in form_frame.winfo_children():
+        widget.configure(bg="#333",fg="#fff", insertbackground="#fff")
+    submit_button.configure(style="DarkButton.TButton")
+
+def set_light_mode():
+    style.theme_use('clam')
+    root.configure(bg="#fff")
+    form_frame.configure(bg="#fff")
+    for widget in form_frame.winfo_children():
+        widget.configure(bg="#fff",fg="#000", insertbackground="#000")
+    submit_button.configure(style="LightButton.TButton")
+
+#Add a menu to switch between dark and light mode
+menu_bar = tk.Menu(root)
+mode_menu = tk.Menu(menu_bar,tearoff=0)
+mode_menu.add_command(label="Dark Mode",command=set_dark_mode)
+mode_menu.add_command(label="Light Mode",command=set_light_mode)
+menu_bar.add_cascade(label="Mode",menu=mode_menu)
+root.configure(menu=menu_bar)
+
+#Load data from Excel
+
+def load_data():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        wb = xl.load_workbook(file_path)
+        ws = wb.active
+        for row in ws.iter_rows(values_only=True):
+            print(row)
+load_button = ttk.Button(form_frame,text="Load Data",command=load_data)
+load_button.grid(row=6,columnspan=2,sticky="EW")
+
+#Add a treeview to display the data
+data_tree = ttk.Treeview(display_frame)
+data_tree.pack()
+
+#add visualisation to the data
+def visualise_data():
+    import matplotlib.pyplot as plt
+    wb = xl.load_workbook("data.xlsx")
+    ws = wb.active
+    data = []
+    for row in ws.iter_rows(values_only=True):
+        data.append(row)
+    
+    #create a bar chart
+    plt.bar(range(len(data)),[row[1] for row in data])
+    plt.title("Age distribution")
+    plt.xlabel("Person")
+    plt.ylabel("Age")
+    plt.show()
+visualize_button = ttk.Button(display_frame,text="Visualize Data",command=visualise_data)
+visualize_button.pack()
+#Start the application
+root.mainloop()
