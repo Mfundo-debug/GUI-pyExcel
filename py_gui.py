3d69da9a29cf467db66742e8e819ed385113da01
Mfundo-debug <diditmfundo@gmail.com>
2023-08-07 12:38:21 +0200

    issues with switching modes

--- py_gui.py
+++ py_gui.py
@@ -1,3 +1,12 @@
+"""
+This program creates a GUI application using tkinter to collect user data and store it in an Excel file. The program also allows the user to load data from an existing Excel file, display the data in a treeview, and visualize the data using matplotlib.
+
+The program consists of two frames: a data entry form and a data display frame. The data entry form contains labels and entry fields for the user to input their name, age, gender, and hobbies. The form also includes a checkbox for the user to agree to the terms and conditions, and a submit button to add the data to the Excel file.
+
+The program also includes a dark mode and light mode, which can be toggled using a menu. The program loads data from an existing Excel file using a load button, and displays the data in a treeview in the data display frame. The program also includes a visualize button to create a bar chart of the age distribution of the data using matplotlib.
+
+The program is designed to be user-friendly and easy to use, with clear labels and error messages to guide the user through the data entry process.
+"""
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog, messagebox
